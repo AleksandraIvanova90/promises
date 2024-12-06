2b75dc6c75f57f37f7b748f3974c20f5f6e246b8
Aleksandra Ivanova <sashaivanova90@mail.ru>
2024-12-06 12:14:53 +0300

    Second commit

--- src/js/gameSavingLoader.js
+++ src/js/gameSavingLoader.js
@@ -2,17 +2,15 @@ import json from '../parser.js';
 import read from '../reader.js';
 
 export default class GameSavingLoader {
-    static load() {
-        return new Promise((resolve) => {
-            const data = read();
-            data.then((response) => {
-              const value = json(response);
-              return value;
-            }).then((value) => {
-              resolve(value);
-            });
-          });
-        }
-      }
+  static load() {
+    const data = read();
+    data.then((response) => {
+    return json(response);
+    }).then((value) => {
+      resolve(value);
+    });
+  };
+}
+
     
    
